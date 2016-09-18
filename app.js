01ec4f6dd5d76ed9241d0ba4e4743917461a74fb
preeti1978 <lekshmy.money@gmail.com>
2016-09-18 13:27:07 +0530

    javascript console print

---
+++ app.js
@@ -0,0 +1,8 @@
+//In javascript file, I am declaring variables (as asked in Assignment Question) to store my details
+let  name="preeti", age=38, dateofbirth="12-07-1978" ,placeofbirth="Trivandrum";
+
+//displaying values in console using console.log function
+console.log("My name="+name);
+console.log("My age="+age);
+console.log("My DOB="+dateofbirth);
+console.log("My placeofbirth="+placeofbirth);
